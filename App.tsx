c4392ca5852fec7fdb7d400968958bd6781d19fa
Lucas da Silva <sklucassilva@gmail.com>
2024-10-01 20:22:33 -0300

    implementation contextApi

--- App.tsx
+++ App.tsx
@@ -3,14 +3,17 @@ import { createStackNavigator } from '@react-navigation/stack';
 import StackNavigator from './src/routes/Stack';
 import { StatusBar } from 'react-native';
 import color from './src/styles/colors';
+import { AuthProvider } from './src/context/AuthContext';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar barStyle="light-content" backgroundColor={color.bg} />
-      <StackNavigator />
-    </NavigationContainer>
+    <AuthProvider>
+      <NavigationContainer>
+        <StatusBar barStyle="light-content" backgroundColor={color.bg} />
+        <StackNavigator />
+      </NavigationContainer>
+    </AuthProvider>
   );
 }
--- src/routes/Stack.tsx
+++ src/routes/Stack.tsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../pages/Home';
 import Tabs from './Tabs';
+import { AuthContext } from '../context/AuthContext';
 
 const Stack = createStackNavigator();
 
 export default function StackNavigator() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+  const authContext = useContext(AuthContext); // Acessa o contexto
+  const isLoggedIn = authContext?.isLoggedIn; // Pega a propriedade isLoggedIn 
+  
   return (
     <Stack.Navigator>
       {!isLoggedIn ? (
